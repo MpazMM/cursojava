9f81e220b424851bf37f4619c2f3fd1c4fca468e
humbersanme <geomar8710@gmail.com>
2023-11-29 20:47:29 +0100

    ejercicio oracle

--- cursojava/src/hibernate/ejercicio2/Consulta.java
+++ cursojava/src/hibernate/ejercicio2/Consulta.java
@@ -5,18 +5,41 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import hibernate.ejercicio.entities.Usuario;
+import hibernate.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+
 public class Consulta {
 	
 	private static final Logger logger = LogManager.getLogger(Consulta.class);
 
 	public static void main(String[] args) {
-		
+		EntityManager em = JpaUtil.getEM("hibernateOracle");
+        Usuario usuario1 = new Usuario("Nombre1","Apellido1","DNI1", obtenerFechaActual());
+        Usuario usuario2 = new Usuario("Nombre2","Apellido2","DNI2", obtenerFechaActual());
+        Usuario usuario3 = new Usuario("Nombre3","Apellido3","DNI3", obtenerFechaActual());
+        Usuario usuario4 = new Usuario("Nombre4","Apellido4","DNI4", nuevaFechaActual(2014));
+        
+        em.getTransaction().begin();
+        em.persist(usuario1);
+        em.persist(usuario2);
+        em.persist(usuario3);
+        em.persist(usuario4);
+        em.getTransaction().commit();
+        
+        List<UsuarioOracle> usuarios = em.createQuery("from UsuarioOracle", UsuarioOracle.class).getResultList();
+        usuarios.forEach(System.out::println);//Método Referencia
+        //clientes.forEach(cliente->System.out.println(cliente));
+        em.close();
+        logger.debug("Final");
+        
 		Consulta cm = new Consulta();
 		UsuarioHibernateDAO userDao = new UsuarioHibernateDAO();
 		cm.crearArchivo("./recursos/consultausuarios.txt",userDao);
@@ -24,8 +47,6 @@ public class Consulta {
 	}
 
 
-	
-	
 	
 	public void crearArchivo(String nombre, UsuarioHibernateDAO usuarioDAO) {
 		File archivo = new File(nombre);
@@ -61,4 +82,13 @@ public class Consulta {
 		}
 	}
 
+	  private static Date obtenerFechaActual() {
+	        return Calendar.getInstance().getTime();
+	    }
+	  
+	  private static Date nuevaFechaActual(int año) {
+  		Calendar c = Calendar.getInstance();
+  		c.set(Calendar.YEAR,año);
+  		return c.getTime();
+	    }
 }
